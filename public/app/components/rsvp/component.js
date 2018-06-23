0bd3a2b8f1da966fde15e272aa648310bb33a43f
thatianaviana <cruz.thatiana@gmail.com>
2018-06-23 13:03:15 -0700

    rsvp not working

--- public/app/components/rsvp/component.js
+++ public/app/components/rsvp/component.js
@@ -5,6 +5,26 @@ angular
     templateUrl: '/app/components/rsvp/rsvp.html',
     bindToController: true //required for $scope to work
 })
-.controller('rsvpController',function($scope){
+.controller('rsvpController',function($scope, $firebaseArray, Notification){
+  //Load firebase guests into $scope;
+  var ref = firebase.database().ref('attendingList');
+  $scope.attendingList = $firebaseArray(ref);
+
+  $scope.attendance = {
+    rsvp: '',
+    name: '',
+    email: ''
+  }
+
+  $scope.submit = function() {
+    console.log("is this working?");
+    ref.push($scope.attendance);
+    $scope.attendance = {
+      rsvp: '',
+      name: '',
+      email: '',
+    };
+    Notification.success({message: "Thanks for the Reply!"})
+}
 
 });
--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -1,30 +1,6 @@
 angular
 .module('app.home')
-.controller('HomeController', function($scope, $firebaseArray, Notification){
-    //Load firebase guests into $scope;
-  var ref = firebase.database().ref('attendingList');
-  $scope.attendingList = $firebaseArray(ref);
+.controller('HomeController', function(){
 
-  $scope.attendance = {
-    rsvp: '',
-    name: '',
-    email: ''
-  }
-
-  $scope.submit = function() {
-    console.log("is this working?");
-    ref.push($scope.attendance);
-    $scope.attendance = {
-      rsvp: '',
-      name: '',
-      email: '',
-    };
-    Notification.success({message: "Thanks for the Reply!"})
-  }
-
-  // $scope.youGotIt = function(){
-  //
-  //   console.log("you got it");
-  // }
 
 });
